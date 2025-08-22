c4bb8dc8662edbaf5cd839e50884afcdf7ba6605
Singh Bhullar <25bhusi@goodnews.vic.edu.au>
2025-08-22 14:37:22 +1000

    Added app.py to project

---
+++ python/login.py
@@ -0,0 +1,12 @@
+
+x=   [line.strip() for line in open('loginorregister.html')]
+username = [line.strip() for line in open('ussername.txt')]
+passowrd = [line.strip() for line in open('password.txt')]
+
+
+if x == username :
+    print ("hi")
+
+    else:
+        x == password:
+        print("no")
